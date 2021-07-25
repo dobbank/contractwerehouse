1d1dba595cf1653c3d7a8be01555c94b2bc2d4c4
dobbank <83172434+dobbank@users.noreply.github.com>
2021-07-25 11:36:28 -0500

    admin/reports/status/php post install

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitbe547292b85cfecf6c4c334dff079de5::getLoader();
+return ComposerAutoloaderInita5ecc8855f40fed0d639d737c6f979a3::getLoader();
